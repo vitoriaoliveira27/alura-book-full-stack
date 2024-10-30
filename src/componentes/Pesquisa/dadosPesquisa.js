4be91f9a60b86f08318a22b09acea304522d9514
Vitoria <vitoriahta27@gmail.com>
2024-10-30 18:54:08 -0300

    Projeto final

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,19 @@
+import livro from '../../imagens/livro.png';
+
+export const livros = [
+    {
+        nome: "Crepúsculo", id: 1, src: livro
+    },
+    {
+        nome: "Amanhecer", id: 2, src: livro
+    },
+    {
+        nome: "Lua Nova", id: 3, src: livro
+    },
+    {
+        nome: "Guia do Mochileiro das Galáxias", id: 4, src: livro
+    },
+    {
+        nome: "Uma família feliz", id: 5, src: livro
+    },
+];
